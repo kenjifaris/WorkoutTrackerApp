53490e5a33b2395c10cbe02be39071203a2a79c9
Josh Faris <joshkfaris@gmail.com>
2024-08-19 13:56:24 -0700

    Initial Commit

---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Kenji  on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
